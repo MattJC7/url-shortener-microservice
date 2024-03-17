70efdc0455e77c7ff4027b8dfa16dbd70e5c1e1f
MattJC7 <mattjcollins7@gmail.com>
2024-03-17 21:08:52 +0000

    add: initial files

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # URL Shortener Microservice
 
-This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+Project 3 of Back End Development through FreeCodeCamp curriculum
+
+## Function
+
+- Accepts a date string (DD Month YYYY), date (YYYY-MM-DD) or unix timestamp and provides a JSON object with unix and UTC properties
+- A blank query returns the current date
+
+## How to use 
+
+```
+
+```
+
+## Learning
+
+- The first argument to Mongoose model constructor defines the name of the MongoDB collection
+
+```
+let Url = mongoose.model('url', urlSchema);
+
+//Collection name becomes 'urls'
+```
+
+- Mongoose queries such as `model.countDocuments()` return a promise. To declare the results of a query in a variable, use `async await`
+- This was useful general JavaScript learning, but also database/back end related
+
+```
+async (req, res) => {
+  const count = await Url.countDocuments({}).then((count) => {return count}).catch((err) => console.log(err))
+}
+
+//Note that the .then() and .catch() aren't strictly needed to return the value. I commented out the .then() but left in the .catch() for error handling purposes
+```
+
+- A general debugging issue I came across was in lines 40, 41 and 46, 47. On first pass, I'd named the JSON properties in `res.json()` as required by FreeCodeCamp but also named the properties for my MongoDB with these values, which was different to my schema. In hindsight, I should have aligned the schema with the FreeCodeCamp requirements.
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
+const bodyParser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+let mongoose = require('mongoose');
+
+mongoose.connect(process.env.URI, {useNewUrlParser: true, useUnifiedTOpology: true});
+
+const urlSchema = new mongoose.Schema({
+  originalUrl: String,
+  newUrl: Number,
+});
+
+let Url = mongoose.model('url', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,10 +28,41 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.post('/api/shorturl', urlencodedParser,  async (req, res) => {
+  const count = await Url.countDocuments({})/* .then((count) => {return count}) */.catch((err) => console.log(err))
+  const urlPosted = req.body.url
+  const urlCount = count + 1
+  const urlObject = new URL(urlPosted)
+
+  dns.lookup(urlObject.hostname, (err, add, fam) => {
+    if(!err) {
+      let url = new Url ({
+        originalUrl: urlPosted,
+        newUrl: urlCount
+      })
+      url.save()/* .then(savedDoc => console.log(savedDoc)) */
+    
+      res.json({
+        original_url: urlPosted,
+        short_url: urlCount
+      })
+    }else {
+      res.json({
+        error: 'invalid url'
+      })
+    }
+  })
+})
+
+app.get('/api/shorturl/:number', (req, res) => {
+  Url
+    .findOne({newUrl: req.params.number})
+    .then((found) => {
+      res.redirect(found.originalUrl)
+    })
+    .catch((err) => console.log(err))
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
